3681335ac2b3162fba486b5e560883835e03ea00
hdi10 <s0551006@htw-berlin.de>
2022-11-23 05:38:05 +0100

    tabLayout with fragments as viewPager implemented

--- app/src/main/java/com/example/randomchoicehelperapplicationapp/FindTheBestChoice.java
+++ app/src/main/java/com/example/randomchoicehelperapplicationapp/FindTheBestChoice.java
@@ -1,22 +1,37 @@
 package com.example.randomchoicehelperapplicationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
 
 import android.os.Bundle;
+import android.util.Log;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class FindTheBestChoice extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_the_best_choice);
 
-        //BottomNavigationView.OnNavigationItemReselectedListener { item ->
-         //   when(item.itemID)
+        tabLayout =findViewById(R.id.tablayout);
+        viewPager = findViewById(R.id.viewpager);
 
+        tabLayout.setupWithViewPager(viewPager);
+
+        Log.d("","huier");
+
+        VPAdapter vpAdapter = new VPAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addFragment(new Fragment1(),"Mondays*Produktgruppe1**");
+        vpAdapter.addFragment(new Fragment2(),"Tuesdays*Produktgruppe2**");
+        vpAdapter.addFragment(new Fragment3(),"Wednesdays*Produktgrupp3**");
+        viewPager.setAdapter(vpAdapter);
 
     }
 }
